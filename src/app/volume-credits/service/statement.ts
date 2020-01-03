29f6c8cdaed570b2e62de369420f63fd376c9987
jpoyard <jpoyard@oxiane.com>
2020-01-03 12:43:31 +0100

    use of reduce in totalAmount and totalVolumeCredits

--- src/app/volume-credits/service/statement.ts
+++ src/app/volume-credits/service/statement.ts
@@ -50,19 +50,11 @@ export function statement(invoice, plays) {
     }
 
     function totalAmount(data: { performances; }) {
-        let result = 0;
-        for (let perf of data.performances) {
-            result += perf.amount;
-        }
-        return result;
+        return data.performances.reduce((total, p) => total + p.amount, 0);
     }
 
     function totalVolumeCredits(data: { performances; }) {
-        let result = 0;
-        for (let perf of data.performances) {
-            result += (perf.volumeCredits);
-        }
-        return result;
+        return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
     }
 }
 
