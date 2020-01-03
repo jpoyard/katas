cef603f398756448f5ed73ed01e049d20e2c6207
jpoyard <jpoyard@oxiane.com>
2020-01-03 12:31:29 +0100

    move playFor

--- src/app/volume-credits/service/statement.ts
+++ src/app/volume-credits/service/statement.ts
@@ -1,13 +1,25 @@
 export function statement(invoice, plays) {
-    const statementData = {};
-    return renderPlainText(statementData, invoice, plays);
+    const statementData: any = {};
+    statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances.map(enrichPerformance);
+    return renderPlainText(statementData);
+
+    function enrichPerformance(aPerformance) {
+        const result = {...aPerformance};
+        result.play = playFor(aPerformance);
+        return result;
+    }
+
+    function playFor(aPerformance: { playID: string }) {
+        return plays[aPerformance.playID];
+    }
 }
 
-function renderPlainText(data, invoice, plays) {
-    let result = `Statement for ${invoice.customer}\n`;
-    for (let perf of invoice.performances) {
+function renderPlainText(data: { customer: string, performances }) {
+    let result = `Statement for ${data.customer}\n`;
+    for (let perf of data.performances) {
         // print line for this order
-        result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
+        result += `  ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     }
     result += `Amount owed is ${usd(totalAmount())}\n`;
     result += `You earned ${totalVolumeCredits()} credits\n`;
@@ -15,7 +27,7 @@ function renderPlainText(data, invoice, plays) {
 
     function totalAmount() {
         let result = 0;
-        for (let perf of invoice.performances) {
+        for (let perf of data.performances) {
             result += amountFor(perf);
         }
         return result;
@@ -23,7 +35,7 @@ function renderPlainText(data, invoice, plays) {
 
     function totalVolumeCredits() {
         let result = 0;
-        for (let perf of invoice.performances) {
+        for (let perf of data.performances) {
             result += volumeCreditsFor(perf);
         }
         return result;
@@ -37,21 +49,18 @@ function renderPlainText(data, invoice, plays) {
             }).format(amount / 100);
     }
 
-    function volumeCreditsFor(aPerformance: { playID: string; audience: number }): number {
+    function volumeCreditsFor(aPerformance: { playID: string; audience: number, play: { type: string } }): number {
         let result = 0;
         result += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+        if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
         return result;
     }
 
-    function playFor(perf: { playID: string }) {
-        return plays[perf.playID];
-    }
 
     function amountFor(aPerformance) {
         let result = 0;
 
-        switch (playFor(aPerformance).type) {
+        switch ((aPerformance.play).type) {
             case "tragedy":
                 result = 40000;
                 if (aPerformance.audience > 30) {
@@ -66,7 +75,7 @@ function renderPlainText(data, invoice, plays) {
                 result += 300 * aPerformance.audience;
                 break;
             default:
-                throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+                throw new Error(`unknown type: ${aPerformance.play.type}`);
         }
 
         return result
