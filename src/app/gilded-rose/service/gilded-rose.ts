c7741f6117bc5d8a19fc61a63acc278ef3e9859e
jpoyard <jpoyard@oxiane.com>
2020-01-03 21:23:49 +0100

    Code ready for polymorphism

--- src/app/gilded-rose/service/gilded-rose.ts
+++ src/app/gilded-rose/service/gilded-rose.ts
@@ -18,57 +18,43 @@ export class GildedRose {
     }
 
     private static updateItemQuality(item: Item) {
-        GildedRose.Q(item);
-        return item;
-    }
-
-    private static Q(item: Item) {
-        if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.quality > 0) {
-                if (item.name != 'Sulfuras, Hand of Ragnaros') {
-                    item.quality = item.quality - 1
-                }
-            }
-        } else {
+        if (item.name === 'Aged Brie') {
+            item.sellIn = item.sellIn - 1;
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
-                if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1
-                        }
-                    }
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1
-                        }
-                    }
-                }
             }
-        }
-        if (item.name != 'Sulfuras, Hand of Ragnaros') {
+            if ((item.sellIn < 0) && (item.quality < 50)) {
+                item.quality = item.quality + 1
+            }
+        } else if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
             item.sellIn = item.sellIn - 1;
-        }
-        if (item.sellIn < 0) {
-            if (item.name != 'Aged Brie') {
-                if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-                    if (item.quality > 0) {
-                        if (item.name != 'Sulfuras, Hand of Ragnaros') {
-                            item.quality = item.quality - 1
-                        }
-                    }
-                } else {
-                    item.quality = item.quality - item.quality
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+                if ((item.sellIn < 10) && (item.quality < 50)) {
+                    item.quality = item.quality + 1
                 }
-            } else {
-                if (item.quality < 50) {
+                if ((item.sellIn < 5) && (item.quality < 50)) {
                     item.quality = item.quality + 1
                 }
             }
+            if (item.sellIn < 0) {
+                item.quality = 0
+            }
+        } else if (item.name === 'Sulfuras, Hand of Ragnaros') {
+            // NO ACTION
+        } else {
+            item.sellIn = item.sellIn - 1;
+            if (item.quality > 0) {
+                item.quality = item.quality - 1
+            }
+            if ((item.sellIn < 0) && (item.quality > 0)) {
+                item.quality = item.quality - 1
+            }
         }
+        return item;
     }
 
-    updateQuality() {
+    public updateQuality() {
         for (let i = 0; i < this.items.length; i++) {
             this.items = this.items.map(item => {
                 return GildedRose.updateItemQuality(item);
