4fed833a03926536bdacea6633535b9a6d9b4a76
jpoyard <jpoyard@oxiane.com>
2020-01-03 12:18:15 +0100

    renderPlainText

--- src/app/volume-credits/service/statement.ts
+++ src/app/volume-credits/service/statement.ts
@@ -1,28 +1,32 @@
 export function statement(invoice, plays) {
+    const statementData = {};
+    return renderPlainText(statementData, invoice, plays);
+}
+
+function renderPlainText(data, invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`;
     for (let perf of invoice.performances) {
         // print line for this order
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     }
-
     result += `Amount owed is ${usd(totalAmount())}\n`;
     result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
 
     function totalAmount() {
-        let totalAmount = 0;
+        let result = 0;
         for (let perf of invoice.performances) {
-            totalAmount += amountFor(perf);
+            result += amountFor(perf);
         }
-        return totalAmount;
+        return result;
     }
 
     function totalVolumeCredits() {
-        let volumeCredits = 0;
+        let result = 0;
         for (let perf of invoice.performances) {
-            volumeCredits += volumeCreditsFor(perf);
+            result += volumeCreditsFor(perf);
         }
-        return volumeCredits;
+        return result;
     }
 
     function usd(amount: number): string {
