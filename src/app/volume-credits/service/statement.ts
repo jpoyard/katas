ce499ae8d2002790fe9a27cb2298b495cfe8ff66
jpoyard <jpoyard@oxiane.com>
2020-01-03 12:35:04 +0100

    move amountFor

--- src/app/volume-credits/service/statement.ts
+++ src/app/volume-credits/service/statement.ts
@@ -6,12 +6,37 @@ export function statement(invoice, plays) {
 
     function enrichPerformance(aPerformance) {
         const result = {...aPerformance};
-        result.play = playFor(aPerformance);
+        result.play = playFor(result);
+        result.amount = amountFor(result);
         return result;
     }
 
     function playFor(aPerformance: { playID: string }) {
         return plays[aPerformance.playID];
+    }
+
+    function amountFor(aPerformance) {
+        let result = 0;
+
+        switch ((aPerformance.play).type) {
+            case "tragedy":
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error(`unknown type: ${aPerformance.play.type}`);
+        }
+
+        return result
     }
 }
 
@@ -19,7 +44,7 @@ function renderPlainText(data: { customer: string, performances }) {
     let result = `Statement for ${data.customer}\n`;
     for (let perf of data.performances) {
         // print line for this order
-        result += `  ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
+        result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
     }
     result += `Amount owed is ${usd(totalAmount())}\n`;
     result += `You earned ${totalVolumeCredits()} credits\n`;
@@ -28,7 +53,7 @@ function renderPlainText(data: { customer: string, performances }) {
     function totalAmount() {
         let result = 0;
         for (let perf of data.performances) {
-            result += amountFor(perf);
+            result += perf.amount;
         }
         return result;
     }
@@ -57,27 +82,4 @@ function renderPlainText(data: { customer: string, performances }) {
     }
 
 
-    function amountFor(aPerformance) {
-        let result = 0;
-
-        switch ((aPerformance.play).type) {
-            case "tragedy":
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${aPerformance.play.type}`);
-        }
-
-        return result
-    }
 }
