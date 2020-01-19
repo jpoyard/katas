135a3fe6eda42cb288e35d81263971cf5551659f
jpoyard <jpoyard@oxiane.com>
2020-01-19 18:00:07 +0100

    ready for test when counter is greater than 9

--- src/app/one-two/service/one-two.class.ts
+++ src/app/one-two/service/one-two.class.ts
@@ -1,7 +1,53 @@
+interface NumberCounter {
+    count: number;
+    value: string;
+}
+
 export class OneTwo {
+    private static readonly i18n = new Map([
+        ['1', 'one'],
+        ['2', 'two'],
+        ['3', 'three'],
+        ['4', 'four'],
+        ['5', 'five'],
+        ['6', 'six'],
+        ['7', 'seven'],
+        ['8', 'eight'],
+        ['9', 'nine'],
+    ]);
 
     public convertNumberToString(
-        numberSequence: string) {
-        return "one one";
+        numberSequence: string
+    ) {
+        const numberCounterArray = this.getNumberCountArray(numberSequence);
+        return numberCounterArray.map(item => this.convertEachNumber(item)).join(' ');
+    }
+
+    private getNumberCountArray(numberSequence: string) {
+        const numberCounterArray: NumberCounter[] = numberSequence
+            .split(' ')
+            .reduce<NumberCounter[]>(
+                (acc: NumberCounter[], cur: string) => {
+                    if (acc.length > 0 && acc[acc.length-1].value === cur) {
+                        acc[acc.length-1].count++;
+                    } else {
+                        acc.push({count: 1, value: cur})
+                    }
+                    return acc;
+                }, []
+            );
+        return numberCounterArray;
+    }
+
+    private convertEachNumber(numberCounter: NumberCounter) {
+        return `${this.getNumberWorld(numberCounter.count.toString())} ${this.getNumberWorld(numberCounter.value)}`
+    }
+
+    private getNumberWorld(numberElement: string): string {
+        if (OneTwo.i18n.has(numberElement)) {
+            return OneTwo.i18n.get(numberElement);
+        } else {
+            throw new Error(`unknown number ${numberElement}`)
+        }
     }
 }
--- test/tdd/one-two.test.ts
+++ test/tdd/one-two.test.ts
@@ -5,7 +5,15 @@ import {OneTwo} from "../../src/app/one-two/service/one-two.class";
 describe('oneTwo function', () => {
     [
         {when: '1', then: 'one one'},
-        {when: '2', then: 'one two'}
+        {when: '2', then: 'one two'},
+        {when: '1 2', then: 'one one one two'},
+        {when: '1 2', then: 'one one one two'},
+        {when: '3 9', then: 'one three one nine'},
+        {when: '2 2', then: 'two two'},
+        {when: '3 9 9 9 8 8', then: 'one three three nine two eight'},
+        {when: '1 1 1 1 1 1 1', then: 'seven one'},
+        {when: '2 4 4 4 6 6 6 6 6', then: 'one two three four five six'},
+        {when: '5 5 5 5 5 5 5 5 5 5 5 5', then: 'nine five three five'}
     ].forEach(
         scenario => {
             it(`
