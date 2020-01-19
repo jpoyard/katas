4df425f823e1236ffaaf1e9b9f387d2ea76faccb
jpoyard <jpoyard@oxiane.com>
2020-01-19 19:03:54 +0100

    step 2 is ready

--- src/app/one-two/service/one-two.class.ts
+++ src/app/one-two/service/one-two.class.ts
@@ -16,6 +16,10 @@ export class OneTwo {
         ['9', 'nine'],
     ]);
 
+    private static readonly i18nWorldToNumberMap = new Map(Array.from(OneTwo.i18nNumberToWorldMap.entries()).map(
+        entry => [entry[1], entry[0]]
+    ));
+
     public convertNumberToString(
         numberSequence: string
     ) {
@@ -23,7 +27,12 @@ export class OneTwo {
         return numberCounterArray.map(item => this.convertEachNumber(item)).join(' ');
     }
 
-    private getNumberCountArray(numberSequence: string) {
+    public convertStringToNumber(worldSequence: string) {
+        const worldSequenceArray = this.getWorldSequenceArray(worldSequence);
+        return worldSequenceArray.map(item => this.getNumberSequence(item)).join(' ');
+    }
+
+    getNumberCountArray(numberSequence: string) {
         const numberCounterArray: NumberCounter[] = numberSequence
             .split(' ')
             .reduce<NumberCounter[]>(
@@ -41,15 +50,46 @@ export class OneTwo {
         return numberCounterArray;
     }
 
-    private convertEachNumber(numberCounter: NumberCounter) {
+    convertEachNumber(numberCounter: NumberCounter): string {
         return `${this.getNumberWorld(numberCounter.count.toString())} ${this.getNumberWorld(numberCounter.value)}`
     }
 
-    private getNumberWorld(numberElement: string): string {
+    getNumberWorld(numberElement: string): string {
         if (OneTwo.i18nNumberToWorldMap.has(numberElement)) {
             return OneTwo.i18nNumberToWorldMap.get(numberElement);
         } else {
             throw new Error(`unknown number ${numberElement}`)
         }
     }
+
+    private getWorldSequenceArray(worldSequence: string) {
+        return worldSequence.split(' ').reduce<NumberCounter[]>(
+            (acc, cur, i) => {
+                if (i % 2 === 0) {
+                    const count = Number.parseInt(
+                        this.getNumberFromWorld(cur), 10
+                    )
+                    acc.push({count, value: ''})
+                } else {
+                    acc[acc.length - 1].value = cur
+                }
+                return acc;
+            }, []
+        );
+    }
+
+    private getNumberFromWorld(world: string): string {
+        if (OneTwo.i18nWorldToNumberMap.has(world)) {
+            return OneTwo.i18nWorldToNumberMap.get(world);
+        } else {
+            throw new Error(`unknow world ${world}`)
+        }
+    }
+
+    private getNumberSequence(numberCounter: NumberCounter): string {
+        const value = this.getNumberFromWorld(numberCounter.value);
+        let result = new Array(numberCounter.count).fill(value).join(' ');
+        return result;
+    }
+
 }
--- test/tdd/one-two.test.ts
+++ test/tdd/one-two.test.ts
@@ -8,7 +8,6 @@ describe(OneTwo.name, () => {
             {when: '1', then: 'one one'},
             {when: '2', then: 'one two'},
             {when: '1 2', then: 'one one one two'},
-            {when: '1 2', then: 'one one one two'},
             {when: '3 9', then: 'one three one nine'},
             {when: '2 2', then: 'two two'},
             {when: '3 9 9 9 8 8', then: 'one three three nine two eight'},
@@ -26,6 +25,36 @@ when parameter is '${scenario.when}'
 
                     // When
                     const actual = oneTwo.convertNumberToString(scenario.when);
+
+                    // Then
+                    expect(actual).eq(scenario.then);
+                });
+            }
+        )
+    });
+
+    describe('convertStringToNumber()', () => {
+        [
+            {when: 'one one', then: '1'},
+            {when: 'one two', then: '2'},
+            {when: 'one one one two', then: '1 2'},
+            {when: 'one three one nine', then: '3 9'},
+            {when: 'two two', then: '2 2'},
+            {when: 'one three three nine two eight', then: '3 9 9 9 8 8'},
+            {when: 'seven one', then: '1 1 1 1 1 1 1'},
+            {when: 'one two three four five six', then: '2 4 4 4 6 6 6 6 6'},
+            {when: 'nine five three five', then: '5 5 5 5 5 5 5 5 5 5 5 5'}
+        ].forEach(
+            scenario => {
+                it(`
+should return '${scenario.then}' 
+when parameter is '${scenario.when}'            
+`, () => {
+                    // Given
+                    const oneTwo = new OneTwo();
+
+                    // When
+                    const actual = oneTwo.convertStringToNumber(scenario.when);
 
                     // Then
                     expect(actual).eq(scenario.then);
